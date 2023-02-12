6114a35cf1d793305ff7ceee9b90c78c130ff045
kenn <44947639+kennkanja@users.noreply.github.com>
2023-02-12 04:48:58 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//Header  Scroll  COlor Change
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY  > 0);
+}); 
+
+//menu
+let menu  = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+menu.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
